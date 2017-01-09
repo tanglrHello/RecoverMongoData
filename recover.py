44d2dafc5d19214ac81a4ecfbdc0853e037e6bf8
tanglr <tanglr@nlp.nju.edu.cn>
2017-01-09 17:14:01 +0800

    fix

--- recover.py
+++ recover.py
@@ -1,38 +1,34 @@
 import os
 import pymongo
 
+
 def connect_mongodb():
     mongo_ip = "121.40.193.226"
     mongo_port = 27017
     user_name = "root"
     password = "nlpnju"
 
-    mongo_uri = "mongodb://" + user_name + ":" + password + '@'+ mongo_ip + ':' + str(mongo_port)
+    mongo_uri = "mongodb://" + user_name + ":" + password + '@' + mongo_ip + ':' + str(mongo_port)
     mongo_connection = pymongo.Connection(mongo_uri)
 
     return mongo_connection
 
+
 def check_paper():
     paper_in_file_names = os.listdir("./11-5/")
-    print paper_in_file_names
 
     mongo_connection = connect_mongodb()
-    print mongo_connection
-    paper_in_dbs = mongo_connection['GeoPaper']
-    print paper_in_dbs
-
-    paper_in_dbs = paper_in_dbs['ChoiceData']
-    print paper_in_dbs
-
-    paper_in_dbs = paper_in_dbs.find()
-    print paper_in_dbs
+    paper_in_dbs = mongo_connection['GeoPaper']['ChoiceData'].find()
 
     paper_in_dbs_names = []
 
 
     for paper in paper_in_dbs:
-        print paper['testpaperName']
-    print paper_in_dbs_names
+        paper_in_dbs_names.append(paper['testpaperName'])
+
+    for paper_file_name in paper_in_file_names:
+        if paper_file_name not in paper_in_dbs_names:
+            print paper_file_name
 
 check_paper()
 
